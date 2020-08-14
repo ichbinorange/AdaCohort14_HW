2234fda4b1a7ae5ade3fbb4511265f1d500c48e1
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-08-13 22:56:04 -0700

    revision2

--- pre_day3_Number_Guessing_Game.rb
+++ pre_day3_Number_Guessing_Game.rb
@@ -1,22 +1,29 @@
 # generate a random number between 0 – 1000 (including 0, but not including 1000).
 rand_num = rand(1000)
-# assign a variable "total_guess" to calculate the user's total number of guesses.
-total_guess = 0
+# Prompt a user's guess
+print "Guess a Number Between 0-999 ==> "
+guess = gets.chomp
 
-# Repeat a loop until the user make a right guess
-while true do
-  # collect a user's guess
-  print "Guess a Number Between 0-999 ==>"
-  guess = gets.chomp.to_i
+# assign a variable "total_guess" to calculate the user's total number of guesses.
+total_guess = 1
+# A loop runs until the user make a right guess
+until guess.to_i.to_s == guess && guess.to_i == rand_num
+  # Verify a guess ensuring it is a number
+  if guess.to_i.to_s != guess
+    puts "Please enter a number."
+  else
+    # check the user's guess 
+    if guess.to_i < rand_num
+      puts "Higher"
+    elsif guess.to_i > rand_num
+      puts "Lower"
+    end
+  end
+  print "\nGuess a Number Between 0-999 ==> "
+  guess = gets.chomp
   # add 1 to the total_guess everytime the user made one guess
   total_guess += 1
-  # check the user's guess 
-  if guess < rand_num
-    puts "Higher"
-  elsif guess > rand_num
-    puts "Lower"
-  else
-    puts "\nYou nailed it!\nYour total number of guesses is #{ total_guess }" 
-    break
-  end
 end
+
+# Print the total number of guesses
+puts "\nYou nailed it!\nYour total number of guesses is #{ total_guess }" 
